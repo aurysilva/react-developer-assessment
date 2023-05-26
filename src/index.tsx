e6a0337226466f872cdb8b267e9a0c7d2ebde6ba
Aury-Silva <aurysilva2014@gmail.com>
2023-05-26 05:32:14 +0100

    Last clean

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './mock';
-// import styles from './global-style/GlobalStyle.css';
 
 /**
  * This file can be ignored, please work in ./components/App.tsx
